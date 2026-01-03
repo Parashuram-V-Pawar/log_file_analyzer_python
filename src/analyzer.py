cf438c491828e747838b3cd490b0619b9a0718bd
Parashuram <parashurampawar56@gmail.com>
2026-01-03 20:29:19 +0530

    add program:[parashuram] log analyzer

---
+++ src/analyzer.py
@@ -0,0 +1,37 @@
+import re
+import pandas as pd
+
+
+def ip_analyze_log_file(file_path):
+    ip_pattern = re.compile(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
+    ip_count = dict()
+    unique_ips = set()
+
+    status_pattern = re.compile(r'\d{1,3}$')
+    status = dict()
+
+    with open(file_path, 'r') as file:
+        for line in file:
+            ip_address = re.search(ip_pattern, line).group()
+            if ip_address not in unique_ips:
+                unique_ips.add(ip_address)
+                ip_count[ip_address] = 1
+            else:
+                ip_count[ip_address] += 1
+
+            http_status = re.search(status_pattern, line).group()
+            status[http_status] = status.get(http_status, 0) + 1
+
+    with open("src/log_summary.txt","w") as summary:
+        summary.write("IP address summary: \n")
+        summary.write("Ip address         Count\n")
+        for ip_address, count in ip_count.items():
+            summary.write(f"{ip_address:<18}: {count}\n")
+        summary.write("\n\nHTTP Error code summary: \n")
+        summary.write("status code    count\n")
+        for status_code, count in status.items():
+            summary.write(f"{status_code:<13} : {count}\n")
+
+
+if __name__ == "__main__":
+    ip_analyze_log_file('src/server_log.txt')
--- src/log_create.py
+++ src/log_create.py
@@ -1,17 +1,17 @@
 from faker import Faker
-import random
 
 def create_log_data(n):
     f = Faker()
-    with open('server_log.txt', mode='w') as file:
+    with open('src/server_log.txt', mode='w') as file:
         for i in range(n):
             log_line = (
                 f"{f.date_time_this_year().strftime('%Y-%m-%d %H:%M:%S')} "
-                f"{f.random_element(elements =('INFO', 'DEBUG', 'WARNING', 'ERROR', 'CRITICAL'))}"
-                f"{f.sentence(nb_words=6)}"
+                f"{f.random_element(elements = ('INFO', 'DEBUG', 'WARNING', 'ERROR', 'CRITICAL'))} "
+                f"{f.sentence(nb_words=6)} "
                 f"{f.ipv4()} "
-                f"{f.random_elements(elements =(100, 200, 302, 404, 500, 502, 503))}\n"
+                f"{f.random_element(elements = (401, 403, 404, 429, 500, 502, 503))}\n"
             )
             file.write(log_line)
 
-create_log_data(100)
+if __name__ == "__main__":
+    create_log_data(15)
