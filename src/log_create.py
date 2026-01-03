b2248bc1244704a26616bfc2e52d743b57fd0a30
Parashuram <parashurampawar56@gmail.com>
2026-01-03 16:05:57 +0530

    modify program:[parashuram] log create

--- src/log_create.py
+++ src/log_create.py
@@ -7,9 +7,11 @@ def create_log_data(n):
         for i in range(n):
             log_line = (
                 f"{f.date_time_this_year().strftime('%Y-%m-%d %H:%M:%S')} "
-                f"{random.choice(['INFO', 'DEBUG', 'WARNING', 'ERROR', 'CRITICAL'])} "
+                f"{f.random_element(elements =('INFO', 'DEBUG', 'WARNING', 'ERROR', 'CRITICAL'))}"
                 f"{f.sentence(nb_words=6)}"
                 f"{f.ipv4()} "
-                f"{random.choice([200, 404, 500])}\n"
+                f"{f.random_elements(elements =(100, 200, 302, 404, 500, 502, 503))}\n"
             )
             file.write(log_line)
+
+create_log_data(100)
