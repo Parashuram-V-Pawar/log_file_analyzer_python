e8b13c805201f454f6a2f2711a0b10b4c3160d79
Parashuram <parashurampawar56@gmail.com>
2026-01-04 10:16:11 +0530

    modify program:[parashuram] log analyzer

--- src/analyzer.py
+++ src/analyzer.py
@@ -10,18 +10,23 @@ def ip_analyze_log_file(file_path):
     status_pattern = re.compile(r'\d{1,3}$')
     status = dict()
 
-    with open(file_path, 'r') as file:
-        for line in file:
-            ip_address = re.search(ip_pattern, line).group()
-            if ip_address not in unique_ips:
-                unique_ips.add(ip_address)
-                ip_count[ip_address] = 1
-            else:
-                ip_count[ip_address] += 1
+    try:
+        with open(file_path, 'r') as file:
+            for line in file:
+                ip_address = re.search(ip_pattern, line).group()
+                if ip_address not in unique_ips:
+                    unique_ips.add(ip_address)
+                    ip_count[ip_address] = 1
+                else:
+                    ip_count[ip_address] += 1
 
-            http_status = re.search(status_pattern, line).group()
-            status[http_status] = status.get(http_status, 0) + 1
-
+                http_status = re.search(status_pattern, line).group()
+                status[http_status] = status.get(http_status, 0) + 1
+    
+    except:
+        raise FileNotFoundError("Log file not found")
+    
+    
     with open("src/log_summary.txt","w") as summary:
         summary.write("IP address summary: \n")
         summary.write("Ip address         Count\n")
